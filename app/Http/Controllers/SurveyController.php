08c39a2199fc02dbbbf9e3c635a3645d78e601f3
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-11-15 13:48:22 +0330

    Implement survey CRUD

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        $surveys = Survey::where('user_id', $user->id)->paginate();
+        return SurveyResource::collection($surveys);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \App\Http\Requests\StoreSurveyRequest $request
+     * @return SurveyResource
+     */
+    public function store(StoreSurveyRequest $request)
+    {
+        $data = $request->validated();
+        // Check if image was given and save on local file system
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Survey $survey
+     * @return SurveyResource
+     */
+    public function show(Survey $survey)
+    {
+        $user = auth()->user();
+        if ($user->id !== $survey->user_id) {
+            return abort(403, 'Unauthorized action');
+        }
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \App\Http\Requests\UpdateSurveyRequest $request
+     * @param \App\Models\Survey $survey
+     * @return SurveyResource
+     */
+    public function update(UpdateSurveyRequest $request, Survey $survey)
+    {
+        $data = $request->validated();
+
+        // Check if image was given and save on local file system
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+
+            // If there is an old image, delete it
+            if ($survey->image) {
+                $absolutePath = public_path($survey->image);
+                File::delete($absolutePath);
+            }
+        }
+
+        // Update survey in the database
+        $survey->update($data);
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Survey $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Survey $survey)
+    {
+        $user = auth()->user();
+        if ($user->id !== $survey->user_id) {
+            return abort(403, 'Unauthorized action');
+        }
+        $survey->delete();
+        return response('', 204);
+    }
+
+    private function saveImage(mixed $image)
+    {
+        // Check if image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // Take out the base64 encoded text without mime type
+            $image = substr($image, strpos($image, ',') + 1);
+            // Get file extension
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            // Check if file is an image
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('base64_decode failed');
+            }
+        } else {
+            throw new \Exception('did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
+    }
+}
